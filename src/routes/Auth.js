ad254c5412fac1441134f69110436b58daf970a5
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-01-11 04:48:30 +0900

    feat: Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Auth = () => {
-	return <div>Auth</div>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		if (name === 'email') {
+			setEmail(value);
+		} else if (name === 'password') {
+			setPassword(value);
+		}
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+				<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+				<input type="submit" value="Log In" />
+			</form>
+			<div>
+				<button>Continue with Google</button>
+				<button>Continue with Github</button>
+			</div>
+		</div>
+	);
 };
 
 export default Auth;
