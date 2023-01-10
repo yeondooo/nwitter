68c51a92f3deb5cdd0ecf119c78c7300bf0918f9
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-01-11 05:29:56 +0900

    feat: Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const Auth = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [newAccount, setNewAccount] = useState(true);
+	const auth = getAuth();
 
 	const onChange = (e) => {
 		const { name, value } = e.target;
@@ -13,16 +16,27 @@ const Auth = () => {
 		}
 	};
 
-	const onSubmit = (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault();
+		try {
+			let data;
+			if (newAccount) {
+				data = await createUserWithEmailAndPassword(auth, email, password);
+			} else {
+				data = await signInWithEmailAndPassword(auth, email, password);
+			}
+			console.log('data', data);
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
-				<input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+				<input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
 				<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-				<input type="submit" value="Log In" />
+				<input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
 			</form>
 			<div>
 				<button>Continue with Google</button>
