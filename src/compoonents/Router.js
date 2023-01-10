06c4151e09517c895b535f96ea3648c13377d34d
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-01-11 07:22:01 +0900

    feat: Log Out

--- src/compoonents/Router.js
+++ src/compoonents/Router.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from 'routes/Profile';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Navigation from './Navigation';
 
 const AppRouter = ({ isLoggedIn }) => {
 	return (
 		<Router>
+			{isLoggedIn && <Navigation />}
 			<Switch>
 				{isLoggedIn ? (
 					<>
 						<Route exact path="/">
 							<Home />
 						</Route>
+						<Route exact path="/profile">
+							<Profile />
+						</Route>
+						<Redirect form="*" to="/" />
 					</>
 				) : (
-					<Route eaact path="/">
-						<Auth />
-					</Route>
+					<>
+						<Route eaact path="/">
+							<Auth />
+						</Route>
+						<Redirect form="*" to="/" />
+					</>
 				)}
 			</Switch>
 		</Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,16 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
 
 const Profile = () => {
-	return <div>Profile</div>;
+	const auth = getAuth();
+	const onLogOutClick = () => {
+		auth.signOut();
+	};
+	return (
+		<>
+			<button onClick={onLogOutClick}>Log Out</button>
+		</>
+	);
 };
 
 export default Profile;
