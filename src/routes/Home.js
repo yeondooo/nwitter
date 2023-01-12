8325336debc969700b0f307b184ef49e8ec74211
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-01-13 04:05:21 +0900

    feat: Delete and Update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
 import { addDoc, collection, onSnapshot, query, orderBy, serverTimestamp } from 'firebase/firestore';
+import Nweet from 'compoonents/Nweet';
 
 const Home = ({ userObj }) => {
 	const [nweet, setNweet] = useState('');
@@ -40,9 +41,7 @@ const Home = ({ userObj }) => {
 			</form>
 			<div>
 				{nweets.map((nweet) => (
-					<div key={nweet.id}>
-						<h4>{nweet.text}</h4>
-					</div>
+					<Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
 				))}
 			</div>
 		</div>
