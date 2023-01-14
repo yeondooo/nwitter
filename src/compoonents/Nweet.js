85b5fc0a772ba1d46f355ddbe3fd16df96ba6dd3
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-01-14 10:32:32 +0900

    feat: Deleting Files

--- src/compoonents/Nweet.js
+++ src/compoonents/Nweet.js
@@ -1,16 +1,21 @@
-import { dbService } from 'fbase';
+import { dbService, storageService } from 'fbase';
 import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { deleteObject, ref } from 'firebase/storage';
 import React, { useState } from 'react';
 
 const Nweet = ({ nweetObj, isOwner }) => {
 	const [editing, setEditing] = useState(false);
 	const [newNweet, setNewNweet] = useState(nweetObj.text);
 	const NweetTextRef = doc(dbService, 'nweets', `${nweetObj.id}`);
+	const urlRef = ref(storageService, nweetObj.attachmentURL);
 
 	const onDeleteClick = async () => {
 		const ok = window.confirm('Are you sure you want to delete this nweet?');
 		if (ok) {
 			await deleteDoc(NweetTextRef);
+			if (nweetObj.attachmentUrl !== '') {
+				await deleteObject(urlRef);
+			}
 		}
 	};
 
