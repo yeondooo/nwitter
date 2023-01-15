2fa100d3cf438550a978298af879b8dbb456dab4
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-01-15 23:46:13 +0900

    feat: Get My Own Nweets

--- src/compoonents/Router.js
+++ src/compoonents/Router.js
@@ -17,7 +17,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
 							<Home userObj={userObj} />
 						</Route>
 						<Route exact path="/profile">
-							<Profile />
+							<Profile userObj={userObj} />
 						</Route>
 						<Redirect form="*" to="/" />
 					</>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,30 @@
+import { dbService } from 'fbase';
 import { getAuth } from 'firebase/auth';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { collection, getDocs, query, where } from '@firebase/firestore';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
 	const auth = getAuth();
+	const history = useHistory();
+
 	const onLogOutClick = () => {
 		auth.signOut();
+		history.push('/');
 	};
+
+	const getMyNweets = async () => {
+		const q = query(collection(dbService, 'nweets'), where('creatorId', '==', userObj.uid));
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach((doc) => {
+			console.log(doc.id, ' => ', doc.data());
+		});
+	};
+
+	useEffect(() => {
+		getMyNweets();
+	}, []);
+
 	return (
 		<>
 			<button onClick={onLogOutClick}>Log Out</button>
